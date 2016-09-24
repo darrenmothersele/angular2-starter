508fdc224afb22ed0ffe4be023d87b8acabbcd08
Darren <darren@darrenmothersele.com>
2016-09-24 20:17:44 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import 'rxjs';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { AppModule } from './app/app.module';
+
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
